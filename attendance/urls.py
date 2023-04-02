e43d3a34533ceecb2d7974dd6f3191b3120d86ab
vishnu-vashishth <vishnu.cse20@satyug.edu.in>
2023-04-02 03:13:19 -0700

    profile added

--- attendance/urls.py
+++ attendance/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 urlpatterns = [ 
     path('', views.index, name='attendance'),
-    path('', views.profile, name='profile'),
+    path('profile', views.profile, name='profile'),
     path('list/', views.list, name='list'),
     path('fetchStudents/', views.fetchStudents, name='list'),
     path('show_sub_wise_att/', views.show_sub_wise_att, name='show_sub_wise_att'),
